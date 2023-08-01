0bcf68d970cf418dfbdceec5d497505a40b5a23b
54albert54 <126289455+54albert54@user.noreply.github.com>
2023-07-31 20:30:38 -0400

    ready now or never

--- api/config/config.js
+++ api/config/config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 
 const config ={
-  env:process.env.NODE_ENV || "dev",
-  isProd:process.env.NODE_ENV ==="production",
+  env:process.env.NODE_ENV || "production", //dev
+  isProd:true, //process.env.NODE_ENV ==="production"
   port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
